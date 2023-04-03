57cf258ef6f843834b81b03bfe788be6752541d0
Jonathan Rayner <jonathan.j.rayner@gmail.com>
2023-04-03 20:31:49 +0100

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Data Loader Script
+
+This Python script is used for loading data from a specified directory. It takes one required argument and two optional arguments:
+
+## Required Argument
+
+- `--data-dir`: Path to the data directory.
+
+## Optional Arguments
+
+- `--num-workers` (int): Number of subprocesses to use for data loading. 0 means that the data will be loaded in the main process. Default value is 0.
+
+- `--batch-size` (int): How many samples per batch to load. Default value is 1.
+
+Note:, the `shuffle` option is always enabled for data loading.
+
+## Usage
+
+Run the script with the required and optional arguments as needed:
+
+```bash
+python data_loader.py --data-dir /path/to/data --num-workers 4 --batch-size 32
+```
--- interview_orbital_materials/run.py
+++ interview_orbital_materials/run.py
@@ -1,18 +1,32 @@
 import argparse
 
 
-def main(data_dir):
+def main(data_dir: str, num_workers: int, batch_size: int):
     print(f"Data directory: {data_dir}")
+    print(f"Number of workers: {num_workers}")
+    print(f"Batch size: {batch_size}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="A script that requires the --data-dir argument."
+        description="A script that requires the --data-dir argument and accepts optional arguments."
     )
     parser.add_argument(
         "--data-dir", type=str, required=True, help="Path to the data directory."
     )
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=0,
+        help="Number of subprocesses to use for data loading. 0 means that the data will be loaded in the main process. (default: 0)",
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=1,
+        help="How many samples per batch to load (default: 1).",
+    )
 
     args = parser.parse_args()
 
-    main(args.data_dir)
+    main(args.data_dir, args.num_workers, args.batch_size)
